fb5cce8fa21854d6bec4ed765c80ae5d33a37cd0
Osborn Ghampson <osbornghdev@gmail.com>
2023-02-26 12:43:07 +0000

    update

--- gh_blog/content/posts/post-1.md
+++ gh_blog/content/posts/post-1.md
@@ -2,3 +2,5 @@
 title: "Post 1"
 date: 2023-02-26T00:52:03Z
 ---
+
+Hello World
--- gh_blog/deploy.sh
+++ gh_blog/deploy.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-echo "Starting Site Update"
+echo -e"\033[0;32mDeploying Updates To Github...\033[0m"
 
+# Build the project
 hugo -t archie
+
+# Go To Public Folder
 cd public
+
+# Add changes to git
 git add .
-git commit -m "update blog"
+
+msg = "updating site `date`"
+
+if [ $# -eq 1 ]
+	then msg="$1"
+fi
+
+git commit -m "$msg"
+
 git push origin main
+
+cd ..
 
 echo "Site Updated Successfully"
